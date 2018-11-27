6fd8a10ba4f5e619d12ba9ab8e4bba1c50af80e2
Antony Chan <antony.chan@oocl.com>
2018-11-27 16:34:49 +0800

    crcreate class EmployeeResource

---
+++ src/main/java/com/tw/apistackbase/employee/EmployeeResource.java
@@ -0,0 +1,12 @@
+package com.tw.apistackbase.employee;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeResource {
+
+}
