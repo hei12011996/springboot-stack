40b9baa25310cfdf7eeb11173742848c15333f00
Antony Chan <antony.chan@oocl.com>
2018-11-27 16:10:50 +0800

    Create class Employee

---
+++ src/main/java/com/tw/apistackbase/employee/Employee.java
@@ -0,0 +1,17 @@
+package com.tw.apistackbase.employee;
+
+public class Employee {
+    private Long id;
+    private String name;
+    private Integer age;
+    private String gender;
+
+    public Employee(Long id, String name, Integer age, String gender){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+
+}
